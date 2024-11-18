0de11bd3967341022e0a124428715b87998374ec
Muruganveldurai <muruganm1867@gmail.com>
2024-11-18 18:24:43 +0530

    getpost

--- topics/task/server.js
+++ topics/task/server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app=express();
 const bodyparser=require('body-parser');
-app.use(express.json(bodyparser.json()))
+app.use(express.json(bodyparser.json()));
 let products=[
     {id:1,
         product:'t-shirt'
@@ -10,12 +10,22 @@ let products=[
     {id:2,
         product:"jeans"
     }
-    
-
-]
-app.get("/items",function(req,res){
+    ]
+app.get("/",function(req,res){
     res.send(products)
 })
+app.post("/items",function(req,res){
+    const mur={
+        id:req.body.id,
+        product:req.body.product
+    }
+    products.push(mur)
+    console.log(products)
+    res.send(products)
+
+    }
+
+)
 app.listen(5000,()=>{
     console.log("running");
 
