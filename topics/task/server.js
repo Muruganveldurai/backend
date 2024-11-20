0fc972b42d7fe8f75fc799f69b6a60720f88ce5d
Muruganveldurai <muruganm1867@gmail.com>
2024-11-20 22:38:23 +0530

    methods

--- topics/task/server.js
+++ topics/task/server.js
@@ -15,17 +15,42 @@ app.get("/",function(req,res){
     res.send(products)
 })
 app.post("/items",function(req,res){
-    const mur={
+    const products={
         id:req.body.id,
         product:req.body.product
     }
-    products.push(mur)
+    products.push(products)
     console.log(products)
-    res.send(products)
+    res.status(201).send({'data':products,'message':'Item Added','code':201});
 
+    })
+ app.put("/items",function(req,res){
+    const products={
+        id:req.body.id,
+        product:req.body.product
     }
+    console.log(products.product)
+    products.forEach((e)=>{
+        if (e.id==products.id)
+            e.id=products.id
+            e.product=products.product
+        res.status(202).send({'data':product,'message':'put code','code':202});
+    })
+    })
+    app.delete("/items",function(req,res){
+         const products={
+        id:req.body.id,
+        product:req.body.product
+         }
+         products.splice(index,1);
+         res.status(202).send({'data':product,'message':'put code','code':202});
 
-)
+
+        }
+    )
+    
+
+
 app.listen(5000,()=>{
     console.log("running");
 
